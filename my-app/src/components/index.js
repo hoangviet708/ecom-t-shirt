4736eab52fb690e1209d7843c292d0dc5425ce2f
Viet <hoangviet708@gmail.com>
2022-08-03 20:08:04 +0700

    create UI Home Page

--- my-app/src/components/index.js
+++ my-app/src/components/index.js
@@ -3,5 +3,6 @@ import { Content } from "./Content";
 import { Footer } from "./Footer";
 import { ProductList } from "./ProductList";
 import { ProductItem } from "./ProductItem";
+import { Navbar } from "./Navbar";
 
-export { Header, Content, Footer, ProductList, ProductItem };
+export { Header, Content, Footer, ProductList, ProductItem, Navbar };
