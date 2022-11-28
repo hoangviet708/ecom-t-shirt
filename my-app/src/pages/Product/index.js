9ea225ded1a57d957811ae5c51850c111bc3cb24
Viet <hoangviet708@gmail.com>
2022-11-28 08:56:39 +0700

    Feat: responsive device tablet max-width=1024

--- my-app/src/pages/Product/index.js
+++ my-app/src/pages/Product/index.js
@@ -1,24 +1,8 @@
-import { useEffect, useState } from "react";
 import { Header, Footer, Content } from "../../components";
-import { productLists } from "../../common/sampleData";
 import { ProductDetail } from "../../components";
 
 const ProductPage = () => {
-  // eslint-disable-next-line no-unused-vars
-  const [productDetails, setProductDetails] = useState();
-
-  let productId = undefined;
-  if (window.location.pathname.includes("/product")) {
-    productId = window.location.pathname.split("/")[2];
-  }
-
-  useEffect(() => {
-    if (productId) {
-      const product = productLists.find((product) => product.id === productId);
-      window.scrollTo(0, 0);
-      setProductDetails(product);
-    }
-  }, [productId]);
+  window.scrollTo(0, 0);
 
   return (
     <div>
